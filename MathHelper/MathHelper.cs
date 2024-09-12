24729fff8af6dd4345a2419392bdc3e386692c39
Анна <vindanka@gmail.com>
2024-09-12 21:36:32 +0300

    create method for factorial

--- MathHelper/MathHelper.cs
+++ MathHelper/MathHelper.cs
@@ -1,7 +1,16 @@
 ﻿namespace MathHelper
 {
-    public class MathHelper
+    public static class MathHelper
     {
-
+        public static int Factorial(int n)
+        {
+            if (n == 0) return 1; // 0! = 1
+            int fact = n;
+            for (int i = n - 1; i > 0; i--) // n-1 'couse we do not need multiply number by itself
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
     }
 }
