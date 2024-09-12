5f20b9d30c04bb646eea0dc6697894efe0ab8f7c
Анна <vindanka@gmail.com>
2024-09-12 22:01:24 +0300

    create methods to set and get value

--- Storage/Storage.cs
+++ Storage/Storage.cs
@@ -2,6 +2,13 @@
 {
     public class Storage <T>
     {
+        private T _value;
 
+        public void SetItem(T item)
+        {
+            _value = item;
+        }
+
+        public T GetItem() => _value;
     }
 }
