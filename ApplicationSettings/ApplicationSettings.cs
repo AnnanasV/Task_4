907f4165afb140a791a6b576c0cf5085d75488ca
Анна <vindanka@gmail.com>
2024-09-12 21:42:32 +0300

    create DB nested class

--- ApplicationSettings/ApplicationSettings.cs
+++ ApplicationSettings/ApplicationSettings.cs
@@ -2,6 +2,15 @@
 {
     public class ApplicationSettings
     {
-
+        public class DatabaseSettings
+        {
+            private string _dbConnection;
+            private TimeOnly _timeout;
+            public DatabaseSettings(string connection)
+            {
+                _dbConnection = connection;
+                _timeout = new TimeOnly(5000);
+            }
+        }
     }
 }
