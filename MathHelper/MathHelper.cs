024182c870fb998e206a4ecfd25ec9631ebbbd41
Анна <vindanka@gmail.com>
2024-09-12 21:37:26 +0300

    create method for GCD

--- MathHelper/MathHelper.cs
+++ MathHelper/MathHelper.cs
@@ -12,5 +12,17 @@
             }
             return fact;
         }
+
+        public static int GCD(int a, int b)
+        {
+            if (a % b == 0)
+                return b;
+            if (b % a == 0)
+                return a;
+
+            if (a > b)
+                return GCD(a % b, b);
+            return GCD(a, b % a);
+        }
     }
 }
