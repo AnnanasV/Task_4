502873f96977878005e560c4632f13f1acf12918
Анна <vindanka@gmail.com>
2024-09-12 22:04:32 +0300

    connect to main project and check

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿var storage1 = new Storage.Storage<int>();
+storage1.SetItem(17);
+Console.WriteLine(storage1.GetItem());
+
+var storage2 = new Storage.Storage<string>();
+storage2.SetItem("str");
+Console.WriteLine(storage2.GetItem());
