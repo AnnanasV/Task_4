5ec589dc6b8c2449fa7b99e8c840898db8971df3
Анна <vindanka@gmail.com>
2024-09-12 23:11:02 +0300

    create event and timer

--- Timer/Timer.cs
+++ Timer/Timer.cs
@@ -2,6 +2,13 @@
 {
     public class Timer
     {
+        public event Action OnTimeElapsed;
 
+        public void Start(int seconds)
+        {
+            Console.WriteLine("start timer");
+            Thread.Sleep(seconds * 1000);
+            OnTimeElapsed?.Invoke();
+        }
     }
 }
