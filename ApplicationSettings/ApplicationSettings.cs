1d6bc6ffadc46f0a3484662809c6960f193ad8ce
Анна <vindanka@gmail.com>
2024-09-12 21:44:40 +0300

    create and set settings for application

--- ApplicationSettings/ApplicationSettings.cs
+++ ApplicationSettings/ApplicationSettings.cs
@@ -2,6 +2,16 @@
 {
     public class ApplicationSettings
     {
+        private string _programName;
+        private string _programVersion;
+
+        public ApplicationSettings()
+        {
+            _programName = "Program Name";
+            _programVersion = "v2.0";
+            var dbSettings = new DatabaseSettings("connection string");
+        }
+
         public class DatabaseSettings
         {
             private string _dbConnection;
