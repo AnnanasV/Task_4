2b03d2d4cb1e078ae72e40a940f68eb260054fbe
Анна <vindanka@gmail.com>
2024-09-12 23:17:09 +0300

    connect and check if it is working

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Timer.Timer timer = new Timer.Timer();
+
+timer.OnTimeElapsed += Timer_OnTimeElapsed;
+timer.Start(3);
+
+Console.ReadLine();
+
+void Timer_OnTimeElapsed()
+{
+    Console.WriteLine("time is up");
+}
