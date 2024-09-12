2f5deb9112161b4108895b109525655d20e8b1fd
Анна <vindanka@gmail.com>
2024-09-12 21:54:03 +0300

    method to show information

--- ApplicationSettings/ApplicationSettings.cs
+++ ApplicationSettings/ApplicationSettings.cs
@@ -4,23 +4,29 @@
     {
         private string _programName;
         private string _programVersion;
+        private DatabaseSettings dbSettings;
 
         public ApplicationSettings()
         {
             _programName = "Program Name";
             _programVersion = "v2.0";
-            var dbSettings = new DatabaseSettings("connection string");
+            dbSettings = new DatabaseSettings("connection string");
         }
 
         public class DatabaseSettings
         {
-            private string _dbConnection;
+            internal string _dbConnection;
             private TimeOnly _timeout;
             public DatabaseSettings(string connection)
             {
                 _dbConnection = connection;
                 _timeout = new TimeOnly(5000);
             }
+        }
+
+        public string ShowInfo()
+        {
+            return _programName + "\t" + _programVersion + "\n" + dbSettings._dbConnection;
         }
     }
 }
